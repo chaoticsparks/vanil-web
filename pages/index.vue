668477445783bc871ed928a015efa4d5f0ea45f7
chaoticsparks <ilya.andreev.it@gmail.com>
2019-11-24 12:21:24 +0100

    Markup for index page (products section and delivery section)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,81 @@
 <template>
-    <div></div>
+    <div>
+        <div class="product">
+            <div class="product__photo">
+                <img class="product__img" src="" alt="" />
+            </div>
+            <div class="product__info">
+                <h2 class="h2-like h2-like--underlined">
+                    Йогурт с фруктами и ягодами c запеченной гранолой
+                </h2>
+                <span class="product__weight">150 г.</span>
+                <p>
+                    бисквит, творожный крем , топпинг малиново-черничный, ганаш,
+                    голубика и малина, зефир маршмеллоу, малиновый джем и
+                    фисташки
+                </p>
+                <section class="product__buy">
+                    <span class="product__price">135 грн.</span>
+                    <button class="btn">Добавить в корзину</button>
+                </section>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product__photo">
+                <img class="product__img" src="" alt="" />
+            </div>
+            <div class="product__info">
+                <h2 class="h2-like h2-like--underlined">
+                    Йогурт с фруктами и ягодами c запеченной гранолой
+                </h2>
+                <span class="product__weight">150 г.</span>
+                <p>
+                    бисквит, творожный крем , топпинг малиново-черничный, ганаш,
+                    голубика и малина, зефир маршмеллоу, малиновый джем и
+                    фисташки
+                </p>
+                <section class="product__buy">
+                    <span class="product__price">135 грн.</span>
+                    <button class="btn">Добавить в корзину</button>
+                </section>
+            </div>
+        </div>
+        <span class="h1-like">Доставка</span>
+        <p>
+            Доставка по Одессе производится через сервис Bond, а также возможен
+            самовывоз из кафе.
+        </p>
+        <span class="bold-text">Адреса отделений:</span>
+        <table class="locations">
+            <tr>
+                <td>ул. Приморский бульвар, 10</td>
+                <td>
+                    <div class="location">
+                        <a href="" class="location__link">Показать на карте</a
+                        ><img src="" class="location__icon" alt="" />
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>ул. Троицкая, 16</td>
+                <td>
+                    <div class="location">
+                        <a href="" class="location__link">Показать на карте</a
+                        ><img src="" class="location__icon" alt="" />
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>ул. Генерала Петрова, 31/1</td>
+                <td>
+                    <div class="location">
+                        <a href="" class="location__link">Показать на карте</a
+                        ><img src="" class="location__icon" alt="" />
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
