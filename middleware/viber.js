6c066068eb19f51f5fae34f4021f2ff9b6eaabaf
chaoticsparks <ilya.andreev.it@gmail.com>
2019-12-04 00:19:34 +0100

    Viber test

--- middleware/viber.js
+++ middleware/viber.js
@@ -1,9 +1,33 @@
-const { ViberClient } = require('messaging-api-viber');
+import axios from 'axios';
+/* const { ViberClient } = require('messaging-api-viber'); */
 
 export default async function({ route, store }) {
-    const client = ViberClient.connect({
+    /* const client = ViberClient.connect({
         accessToken: '4ab3498feb67d4ea-13b937d35cdef139-8f4d592ff4262b1e'
-    });
+    }); */
+
+    const result = await axios.post(
+        'api/get_account_info',
+        {},
+        {
+            headers: {
+                'X-Viber-Auth-Token':
+                    '4ab3498feb67d4ea-13b937d35cdef139-8f4d592ff4262b1e'
+            }
+        }
+    );
+    console.log(result);
+
+    /*
+  const likes = await axios.post('https://webmention.io/api/mentions.jf2', {
+    params: {
+      target: 'https://jeremywynn.com' + route.fullPath + '/',
+      token: process.env.webmentionsToken,
+      'wm-property': 'like-of',Z
+        'per_page': 20
+}
+});
+*/
 
     /* await client.setWebhook('https://vanil-test.netlify.com', [
       'delivered',
@@ -11,7 +35,7 @@ export default async function({ route, store }) {
       'conversation_started'
   ]); */
 
-    const info = await client.getAccountInfo();
+    /* const info = await client.getAccountInfo();
 
     console.log(info);
 
@@ -19,5 +43,5 @@ export default async function({ route, store }) {
 
     const result = client.broadcastText(members, 'hi');
 
-    console.log(result);
+    console.log(result); */
 }
