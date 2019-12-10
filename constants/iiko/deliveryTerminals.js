71948bd8b588658f59722a2b8058949aa8f9ca19
chaoticsparks <ilya.andreev.it@gmail.com>
2019-12-10 18:55:47 +0100

    delivery terminals

---
+++ constants/iiko/deliveryTerminals.js
@@ -0,0 +1,14 @@
+export const deliveryTerminals = {
+    osipova: {
+        title: 'ул. Осипова, 10',
+        iikoId: 'fd6cf2f9-6687-98a1-016e-ea4821bc001d'
+    },
+    primorskiy: {
+        title: 'ул. Приморский бульвар, 10',
+        iikoId: '049f2ee5-03e8-337b-016e-cc62ffbec1cb'
+    },
+    genPetrova: {
+        title: 'ул. Генерала Петрова, 31/1',
+        iikoId: '049f2ee5-03e8-337b-016e-cc62ffbec0a2'
+    }
+};
