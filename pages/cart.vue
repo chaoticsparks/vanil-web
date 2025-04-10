0bbb24247b6fca97bbff497d027ccd4a25086cc0
Illia_Andreiev <Illia.Andreiev-EXT@LDC.com>
2025-04-10 16:22:44 +0300

    правки, приморский включен

--- pages/cart.vue
+++ pages/cart.vue
@@ -88,7 +88,7 @@
                     v-if="delivery === 'Самовывоз'"
                     class="cart-form__self-delivery"
                 >
-                    <!--                    <label class="radio-container">
+                    <label class="radio-container">
                         <input
                             v-model="address"
                             type="radio"
@@ -100,7 +100,7 @@
                             >ул. Приморський бульвар, 10</span
                         >
                         <span class="radio-container__custom-radio"></span>
-                    </label>-->
+                    </label>
                     <label class="radio-container">
                         <input
                             v-model="address"
